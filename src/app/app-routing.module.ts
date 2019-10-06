b8f58e88f42ea5d38aaa073a7983c3bd6a98eb11
quyctd <quy.dc98@gmail.com>
2019-10-06 23:39:11 +0700

    Error component UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './main/login/login.component';
+import { JoinComponent } from './main/join/join.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'join', component: JoinComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './main/login/login.component';
+import { JoinComponent } from './main/join/join.component';
+import { ErrorComponent } from './common/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    JoinComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
