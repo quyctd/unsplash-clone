80b74a69148f592d247f5acdf497ee2e37031d71
quyctd <quy.dc98@gmail.com>
2019-10-20 18:11:25 +0700

    Added UI navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './main/login/login.component';
 import { JoinComponent } from './main/join/join.component';
+import { HomeComponent } from './main/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent},
   { path: 'login', component: LoginComponent },
   { path: 'join', component: JoinComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './main/login/login.component';
 import { JoinComponent } from './main/join/join.component';
 import { ErrorComponent } from './common/error/error.component';
+import { HomeComponent } from './main/home/home.component';
+import { TopbarComponent } from './main/general/topbar/topbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     JoinComponent,
-    ErrorComponent
+    ErrorComponent,
+    HomeComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
