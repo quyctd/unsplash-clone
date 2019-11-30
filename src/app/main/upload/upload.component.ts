686468ac2a1c5fa3fc1fd8c30b3a6d6e694b00ae
quyctd <quy.dc98@gmail.com>
2019-11-30 15:41:50 +0700

    Added coudinary for uploader

--- src/app/main/upload/upload.component.ts
+++ src/app/main/upload/upload.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
+  files = [1];
+  limit = 10;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  get filesLength() {
+    return this.files.length;
+  }
+
+  get remain() {
+    return this.limit - this.filesLength;
+  }
 }
