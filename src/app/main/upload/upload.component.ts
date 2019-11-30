eec82dfb38f504c1c17ad02435852c15f90455a2
quyctd <quy.dc98@gmail.com>
2019-11-30 15:42:06 +0700

    Revert "Added coudinary for uploader"
    This reverts commit 686468ac2a1c5fa3fc1fd8c30b3a6d6e694b00ae.

--- src/app/main/upload/upload.component.ts
+++ src/app/main/upload/upload.component.ts
@@ -7,19 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  files = [1];
-  limit = 10;
-
   constructor() { }
 
   ngOnInit() {
   }
 
-  get filesLength() {
-    return this.files.length;
-  }
-
-  get remain() {
-    return this.limit - this.filesLength;
-  }
 }
